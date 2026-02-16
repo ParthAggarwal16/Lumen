02b916a5f7c3b6944b3d7947046b65a65cceb85b
ParthAggarwal16 <parth22012006@gmail.com>
2026-02-16 17:29:57 +0530

    frontend basics

--- src/layout/applayout.tsx
+++ src/layout/applayout.tsx
@@ -1,18 +1,3 @@
 // wraps entire app 
 // renders sidebar, header and main content
 // handles routing via main switch
-
-function MyButton ({ title } : {title : string}){
-    return (
-        <button> {title} </button>
-    );
-}
-
-export default function MyApp(){
-    return (
-        <div>
-            <h1> Welcome to my app </h1>
-            <MyButton title = "I am a button"/>
-        </div>
-    );
-}
