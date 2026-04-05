789925d55461b4d65db713208e35ab0e89a3f8d8
ParthAggarwal16 <parth22012006@gmail.com>
2026-04-05 22:50:18 +0530

    feat(ui): initial renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
 //emptying this for now 
+
+
+export default function Home() {
+  return (
+    <article>
+      <h1> Lumen </h1>
+      <p>Lumen is a web based wallet on top of solana</p>
+
+
+    </article>
+  )
+}
