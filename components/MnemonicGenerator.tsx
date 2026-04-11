9540dff75054b0ea39cbed6472f17530e0d3b6dd
ParthAggarwal16 <parth22012006@gmail.com>
2026-04-11 15:57:24 +0530

    feat: adding seed generation logic

--- components/MnemonicGenerator.tsx
+++ components/MnemonicGenerator.tsx
@@ -1,4 +1,21 @@
 // this will generate seed and display it 
 
-//import { mnemonicToSeed } from "bip39"
-//const MnemonicGenerator = () => { }
+import { generateMnemonic } from "bip39"
+import { useState } from "react"
+const MnemonicGenerator = () => {
+  const [mnemonic, setMnemonic] = useState<string>('')
+  const generateSeed = () => {
+    const newMnemonic = generateMnemonic()
+    setMnemonic(newMnemonic)
+  }
+  return (
+    <div>
+      <button onClick={generateSeed}>
+        Generate Seed
+      </button>
+      {mnemonic && <p className="mt-4 text-sm">{mnemonic}</p>}
+    </div>
+  )
+}
+
+export default MnemonicGenerator
