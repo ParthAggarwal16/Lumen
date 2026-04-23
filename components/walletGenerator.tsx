82139ec3c5d5474a1ea64a195df37c5e66db8d4d
ParthAggarwal16 <parth22012006@gmail.com>
2026-04-23 13:39:21 +0530

    initializing wallet generator

--- components/walletGenerator.tsx
+++ components/walletGenerator.tsx
@@ -1,1 +1,12 @@
 // initialising wallet generator 
+
+import MnemonicGenerator from "../components/MnemonicGenerator.tsx"
+import nacl from "tweetnacl"
+import { Keypair } from "@solana/web3.js"
+import { derivePath } from "ed25519-hd-key"
+
+interface wallet {
+  publicKey: string
+  privateKey: string
+  path: string
+}
