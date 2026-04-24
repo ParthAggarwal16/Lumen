67bedd4724d05f2e72423e76d28bec735d0624be
ParthAggarwal16 <parth22012006@gmail.com>
2026-04-24 22:42:41 +0530

    .

--- components/walletGenerator.tsx
+++ components/walletGenerator.tsx
@@ -4,9 +4,17 @@ import MnemonicGenerator from "../components/MnemonicGenerator.tsx"
 import nacl from "tweetnacl"
 import { Keypair } from "@solana/web3.js"
 import { derivePath } from "ed25519-hd-key"
+import { mnemonicToSeed } from "bip39"
+import { useState } from "react"
 
 interface wallet {
   publicKey: string
   privateKey: string
   path: string
+}
+
+const walletGenerator = () => {
+  const [pathTypes, setPathTypes] = useState<string[]>([]);
+  const [wallets, setWallets] = useState<wallet[]>([]);
+  return ()
 }
