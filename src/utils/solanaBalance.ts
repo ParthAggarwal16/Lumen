8862b0c8b98435571d0c561f333b14fd2cbee9e4
ParthAggarwal16 <parth22012006@gmail.com>
2026-05-12 17:25:46 +0530

    fixes

--- src/utils/solanaBalance.ts
+++ src/utils/solanaBalance.ts
@@ -3,19 +3,22 @@
 // and handle errors
 
 import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { PublicKey } from "@solana/web3.js"
 
 export function useSolanaBalance(publicKey: string) {
-  const [balance, setBalance] = useState(null)
+  const [balance, setBalance] = useState<number | null>(null)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchSolanaBalance = async () => {
       const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
 
-      const balanceInLamports = await connection.getBalance(publicKey)
+      const publicKeyObj = new PublicKey(publicKey)
+      const balanceInLamports = await connection.getBalance(publicKeyObj)
 
       const balanceInSolana = balanceInLamports / LAMPORTS_PER_SOL
+      setBalance(balanceInSolana)
     }
     fetchSolanaBalance()
   }, [publicKey])
