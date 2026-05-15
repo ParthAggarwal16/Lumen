697163f8f09a61df6e739d011766d9b0a4c49cac
ParthAggarwal16 <parth22012006@gmail.com>
2026-05-16 00:40:56 +0530

    cleaning up before the full refactor

--- components/walletGenerator.tsx
+++ components/walletGenerator.tsx
@@ -5,18 +5,17 @@ import { Keypair } from "@solana/web3.js"
 import { derivePath } from "ed25519-hd-key"
 import { useWalletStore, Wallet } from "./store/walletStore.ts"
 import { mnemonicToSeedSync } from "bip39"
-import { useState } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { WalletCard } from "./walletCard.tsx"
 
 const WalletGenerator = () => {
   const mnemonic = useWalletStore((state) => state.mnemonic)
-  const [pathTypes, setPathTypes] = useState<string[]>([]);
 
   const wallets = useWalletStore((state) => state.wallets)
   const setWallets = useWalletStore((state) => state.setWallets)
   const [visiblePrivateKeys, setVisiblePrivateKeys] = useState<boolean[]>([])
 
-  const generateWallets = () => {
+  const generateWallets = useCallback(() => {
     if (!mnemonic || mnemonic.trim() === "") {
       alert("Please Generate a seed ")
       return
@@ -27,22 +26,27 @@ const WalletGenerator = () => {
     const newWallets: Wallet[] = []
 
     for (let i = 0; i < 4; i++) {
+
       const path = `m/44'/501'/${i}'/0'`;
       const derivedSeed = derivePath(path, hexSeed).key;
+
       const keypair = Keypair.fromSeed(derivedSeed)
       const publicKey = keypair.publicKey.toBase58()
       const privateKey = Buffer.from(keypair.secretKey).toString('hex')
 
-      const wallet = {
-        privateKey, publicKey, path
-      }
+      const wallet = { privateKey, publicKey, path }
+
       newWallets.push(wallet)
     }
-    setWallets([...wallets, ...newWallets])       //append not replace
-    setVisiblePrivateKeys([...visiblePrivateKeys, ...Array(newWallets.length).fill(false)])
-  }
+    setWallets(newWallets)
+    setVisiblePrivateKeys(Array(newWallets.length).fill(false))
+  }, [mnemonic, setWallets])
 
-
+  useEffect(() => {
+    if (mnemonic && wallets.length === 0) {
+      generateWallets()
+    }
+  }, [mnemonic, generateWallets, wallets.length])
 
   const toggleVisiblePrivateKeys = (index: number) => {
     setVisiblePrivateKeys(
