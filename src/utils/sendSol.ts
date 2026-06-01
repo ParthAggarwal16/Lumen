9621b38b985e7dbc7dee5bd740bcad8f62afcde2
ParthAggarwal16 <parth22012006@gmail.com>
2026-06-01 23:23:37 +0530

    connection and receiver address

--- src/utils/sendSol.ts
+++ src/utils/sendSol.ts
@@ -2,10 +2,13 @@
 // cureentl taking some 
 // inspo from solflare
 
-import { Connection, SystemProgram, Transaction, clusterApiUrl, Keypair } from "@solana/web3.js"
+import { Connection, SystemProgram, Transaction, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js"
 
 export async function sendSol(
   senderKeypair: Keypair,
-  receiverKeypair: Keypair,
+  receiverAddress: string
   amount: number
-)
+) {
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+  const recepeintPublicKey = new PublicKey(receiverAddress)
+}
