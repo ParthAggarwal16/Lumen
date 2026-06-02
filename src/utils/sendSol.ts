770bd79fa952e5e87ba94dc565ac78690aa67546
ParthAggarwal16 <parth22012006@gmail.com>
2026-06-02 13:06:13 +0530

    send solana logic

--- src/utils/sendSol.ts
+++ src/utils/sendSol.ts
@@ -2,21 +2,28 @@
 // cureentl taking some 
 // inspo from solflare
 
-import { Connection, SystemProgram, Transaction, clusterApiUrl, Keypair, PublicKey, sendAndConfirmTransaction } from "@solana/web3.js"
+import { Connection, SystemProgram, Transaction, clusterApiUrl, Keypair, PublicKey, sendAndConfirmTransaction, LAMPORTS_PER_SOL } from "@solana/web3.js"
 
 export async function sendSol(
   senderKeypair: Keypair,
-  receiverAddress: string
+  recepeintAddress: string,
   amount: number
 ) {
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
-  const recepeintPublicKey = new PublicKey(receiverAddress)
+  const recepeintPublicKey = new PublicKey(recepeintAddress)
 
   // Transaction code here:
-  const transaction = new Transaction
+  const transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: senderKeypair.publicKey,
+      toPubkey: recepeintPublicKey,
+      lamports: amount * LAMPORTS_PER_SOL
+    })
+  )
 
   //signature:
   const signature = await sendAndConfirmTransaction(
-    connection, [senderKeypair], transaction
+    connection, transaction, [senderKeypair]
   )
+  return signature
 }
