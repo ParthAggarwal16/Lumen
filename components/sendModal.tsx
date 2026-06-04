36a0d896d6cd43a288a7c649ef8f18c28cb229d3
ParthAggarwal16 <parth22012006@gmail.com>
2026-06-04 17:10:40 +0530

    send modal

--- components/sendModal.tsx
+++ components/sendModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useSolPrice } from "../src/hooks/fetchusd.ts"
 
 interface SendModalProps {
   wallet: {
@@ -10,8 +11,13 @@ interface SendModalProps {
 
 export default function SendModal({ wallet, onclose }: SendModalProps) {
   const [recipient, setrecipient] = useState("")
-  const [Amount, setAmount] = useState("")
+  const [amount, setAmount] = useState("")
   const [error, setError] = useState("")
+  const [loading, setLoading] = useSolPrice(false)
+  const solPrice = useSolPrice()
 
-
+  const usdValue = solPrice && amount ? (solPrice * Number(amount)).toFixed(2) : "0.00"
+  const handleSend = async () => {
+    setLoading(true)
+  }
 }
