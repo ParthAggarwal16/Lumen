6b847dcde57547b6b6ef7d198f4edc5d15be23c0
ParthAggarwal16 <parth22012006@gmail.com>
2026-06-07 17:31:28 +0530

    swap logic initialization

--- src/utils/swapTokens.ts
+++ src/utils/swapTokens.ts
@@ -2,3 +2,15 @@
 //Calls Jupiter API
 //Builds & sends transaction
 //Returns signature or error
+
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+const SOL_MINT = "So11111111111111111111111111111111111111112"
+const USDC_MINT = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"
+
+interface swapParams {
+  account: string,
+  inputToken: "SOL" | "USDC",
+  outputToken: "USDC" | "SOL"
+}
