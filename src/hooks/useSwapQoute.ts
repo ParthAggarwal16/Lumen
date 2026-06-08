32ca93054d44f302855d08215aa15536480fc644
ParthAggarwal16 <parth22012006@gmail.com>
2026-06-08 21:39:45 +0530

    swap hook

--- src/hooks/useSwapQoute.ts
+++ src/hooks/useSwapQoute.ts
@@ -3,3 +3,40 @@
 //Get quote (from Jupiter)
 //Confirm & execute swap
 //Show loading/error states
+
+import { useState, useEffect } from "react";
+import { SOL_MINT, USDC_MINT } from "../utils/swapTokens";
+
+export function useSwapQoute(amount: string, toToken: "SOL" | "USDC", fromToken: "SOL" | "USDC") {
+  const [quote, setQoute] = useState<number | null>(null)
+  useEffect(() => {
+    const fetchQoute = async () => {
+      if (!amount || Number(amount) == 0) {
+        setQoute(null)
+        return
+      }
+      try {
+        const inputMint = fromToken === "SOL" ? SOL_MINT : USDC_MINT
+        const outputMint = toToken === "SOL" ? SOL_MINT : USDC_MINT
+        const amountInSmallesUnit = fromToken === "SOL" ? Number(amount) * 1_000_000_000 : Number(amount) * 1_000_000
+
+        const response = await fetch(`https://lite-api.jup.ag/swap/v1/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amountInSmallesUnit}&slippageBps=50`)
+        if (!response.ok) {
+          throw new Error("Failed to fetch quote")
+        }
+        const data = await response.json()
+
+        if (!data.outAmount) {
+          throw new Error("No swap route found")
+        }
+
+        const outAmount = toToken === "SOL" ? Number(data.outAmount) / 1_000_000_000 : Number(data.outAmount) / 1_000_000
+        setQoute(outAmount)
+      } catch {
+        setQoute(null)
+      }
+    }
+    fetchQoute()
+  }, [amount, toToken, fromToken])
+  return quote
+}
